8330a3ea7c3555a6a308aafb6f48f87afde9c4ce
FideleLesobre2022 <fidelelesobre@gmail.com>
2025-01-27 18:24:10 +0200

    Initial commit

--- AndroidHttpClient/app/build.gradle.kts
+++ AndroidHttpClient/app/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.androidhttpclient"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.androidhttpclient"
@@ -37,7 +39,6 @@ android {
     buildFeatures {
         compose = true
     }
-    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
@@ -57,4 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
 }
